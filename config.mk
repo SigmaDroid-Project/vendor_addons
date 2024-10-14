90fe561383e0dc3893b31257a5c146888fc84275
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-14 19:26:54 +0800

    Add HideClock Overlay

--- config.mk
+++ config.mk
@@ -369,7 +369,8 @@ PRODUCT_PACKAGES += \
 # Utility Overlays
 PRODUCT_PACKAGES += \
     HideSmartSpace \
-    SmartSpaceOffset
+    SmartSpaceOffset \
+    HideClock
 
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
